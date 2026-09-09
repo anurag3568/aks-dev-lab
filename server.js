b05ca96273b4db6e2e18798f32283c92bf6134f0
anurag3568 <anuragsingh66kk@gmail.com>
2026-09-10 00:28:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-app.use(express.json());
-app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // MongoDB Schemas
 const VideoSchema = new mongoose.Schema({
